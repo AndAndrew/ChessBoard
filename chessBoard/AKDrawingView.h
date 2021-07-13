79824a90ca6e450675fd8b903cb35387b729f84d
Andrey <andrew.krivokhizhin@gmail.com>
2021-07-13 15:41:26 +0400

    feat: add chessBoardView

---
+++ chessBoard/AKDrawingView.h
@@ -0,0 +1,16 @@
+//
+//  AKDrawingView.h
+//  chessBoard
+//
+//  Created by Andrey Krivokhizhin on 13.07.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AKDrawingView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ chessBoard/AKDrawingView.m
@@ -0,0 +1,20 @@
+//
+//  AKDrawingView.m
+//  chessBoard
+//
+//  Created by Andrey Krivokhizhin on 13.07.2021.
+//
+
+#import "AKDrawingView.h"
+
+@implementation AKDrawingView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
